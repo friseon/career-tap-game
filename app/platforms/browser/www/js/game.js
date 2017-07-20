d317e33507b83020d55c75cfa73bd5e966bbe810
Максим Лыков <frise.on@gmail.com>
2017-07-20 22:46:39 +0300

    pre effect divas

--- app/platforms/browser/www/js/game.js
+++ app/platforms/browser/www/js/game.js
@@ -1,13 +1,39 @@
 var Game = function(options) {
     var canvas;
     var context;
+    var button_restart;
     // препятствия
-    var obstacles = {};
+    var allObjects = {};
+
+    function GameObject(type, name, energy, color, effect) {
+        this.type = type;
+        this.name = name;
+        this.energy = energy;
+        this.color = color;
+        this.effect = effect;
+    }
+    var coffe = new GameObject("objectText", "Кофе", 15, "black");
+    var beer = new GameObject("objectText", "Пивас", 15, "black", { speed: 10, time: 5 });
+    var deadline = new GameObject("objectText", "Дедлайн", -10, "red");
+    var hotfix = new GameObject("objectText", "Хотфикс!", -10, "red");
+    var admin = new GameObject("objectText", "Админ смотрит историю твоего браузера", -10, "red");
+    var information = new GameObject("text", "Игровая информация", 0, "red");
+    var types = [
+        coffe,
+        beer,
+        deadline,
+        deadline,
+        deadline,
+        hotfix,
+        hotfix,
+        hotfix,
+        hotfix,
+        hotfix,
+        admin
+    ];
+
     // игрок
-    var player = {
-        gravity: 0.05,
-        energy: 100
-    };
+    var playerG = new GameObject("player", "Я", 100, "black");
     // очки
     var playerScore = {},
         playerEnergy = {};
@@ -21,78 +47,101 @@ var Game = function(options) {
             x: 80,
             y: 10
         },
-        scale: (window.innerWidth * window.devicePixelRatio || 1) / 800 * (window.innerHeight * window.devicePixelRatio || 1) / 400
+        scale: (window.innerWidth) / 800 * (window.innerHeight) / 400
     }, options);
     // создание игры
     this.create = function() {
         canvas = document.createElement("canvas");
+        button_restart = document.createElement("button");
+        button_restart.innerText = "Restart";
+        button_restart.classList.add("game__button-restart");
         context = canvas.getContext("2d");
-        addEventListeners('touchstart pointerdown', canvas, function(e) {
+        addEventListeners('touchend pointerup', button_restart, function(event) {
+            event.preventDefault();
+            gameArea.restart();
+        });
+        addEventListeners('touchstart pointerdown', canvas, function(evente) {
+            event.preventDefault();
             accelerate(-0.2);
         });
-        addEventListeners('touchend pointerup', canvas, function(e) {
+        addEventListeners('touchend pointerup', canvas, function(evente) {
+            event.preventDefault();
             accelerate(0.05);
         });
         canvas.classList.add('game__canvas');
         document.body.appendChild(canvas);
+        document.body.appendChild(button_restart)
         // canvas.width = this._options.canvasProperties.width;
         // canvas.height = this._options.canvasProperties.height;
-        canvas.width = window.innerWidth * window.devicePixelRatio || 1;
-        canvas.height = window.innerHeight * window.devicePixelRatio || 1;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
         // window.addEventListener('resize', resizeCanvas, false);
         // context.scale(scale.w, scale.h);
     };
+    this.restart = function() {
+        gameArea.restart();
+    };
     // начало игры
     this.startGame = function() {
-        player = Object.assign(new Сomponent(30, 30, "black", _options.startPosition.x, _options.startPosition.y, "player"), player);
-        // player.gravity = 0.05;
-        playerScore = new Сomponent("30px", "30px", "red", 20, 20, "text");
-        playerEnergy = new Сomponent("30px", "30px", "red", 20, 40, "text");
+        player = new Сomponent(30, 30, playerG, _options.startPosition.x, _options.startPosition.y);
+        player.gravity = 0.05;
+        player.speed = 2;
+        playerScore = new Сomponent("30px", "30px", information, 20, 20);
+        playerEnergy = new Сomponent("30px", "30px", information, 20, 40);
         playerEnergy.text = "Energy: " + player.energy;
         gameArea.start();
     };
-    // мир игры
+    // миро игры
     var gameArea = {
         start : function() {
             this.frameNo = 0;
             this.interval = setInterval(updateGameArea, 20);
-            
-            },
+        },
         clear : function() {
             context.clearRect(0, 0, canvas.width, canvas.height);
         },
-        restart: function() { 
-            // canvasProperties.width = canvas.width || 800;
+        restart: function() {
+            player.energy = 100;
+            player.gravity = 0.05;
+            player.x = _options.startPosition.x;
+            player.y = _options.startPosition.y;
+            player.gravitySpeed = 0;
             this.frameNo = 0;
-            obstacles = {};
+            allObjects = {};
             this.clear();
         }
     };
     // конструктор объекта игры
-    function Сomponent(width, height, color, x, y, type) {
-        this.type = type;
-        this.score = 0;
-        if (this.type !== "player") {
-            this.width = width * _options.scale;
-            this.height = height * _options.scale;
-        } else {
-            this.width = this.height = height * _options.scale;
+    function Сomponent(width, height, gameObject, x, y) {
+        this.energy = gameObject.energy;
+        this.width = width;
+        this.height = height;
+        this.gravitySpeed = 0;
+       
+        var fontSize = 30 * _options.scale > 60 ? 60 : 30;
+        this.height = parseInt(fontSize, 10) + 10;
+        this.width = context.measureText(gameObject.name).width + 10;
+
+        if (gameObject.type === "player") {
+            this.width = this.height = parseInt(fontSize, 10) + 10;
         }
         this.speedX = 0;
         this.speedY = 0;
         this.x = x;
         this.y = y;
-        this.gravity = 0;
-        this.gravitySpeed = 0;
         // обновление объекта
         this.update = function() {
-            if (this.type == "text") {
-                context.font = this.width + " " + this.height;
-                context.fillStyle = color;
+            if (gameObject.type == "text") {
+                context.fillStyle = gameObject.color;
+                context.font = fontSize / 4 + "px Arial";
                 context.fillText(this.text, this.x, this.y);
             } else {
-                context.fillStyle = color;
+                context.font = fontSize + "px Arial";
+                context.fillStyle = gameObject.color;
                 context.fillRect(this.x, this.y, this.width, this.height);
+                context.textBaseline = 'top';
+                context.fillStyle = "white";
+                context.fillText(gameObject.name, this.x + 5, this.y);
             }
         };
         // новое положение
@@ -125,58 +174,102 @@ var Game = function(options) {
             var othertop = otherobj.y;
             var otherbottom = otherobj.y + (otherobj.height);
             var crash = true;
-            if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
+            if (otherobj.type === "active" && this.type === "active") {
+                if ( (mybottom < othertop) || (mytop > otherbottom) && (mytop > othertop)) {
+                    crash = false;
+                }
+                return crash;
+            }
+            
+            else if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
                 crash = false;
             }
             return crash;
         }
     }
+    // процесс игры
     var indexObject = 0;
-    var deleteObject;
-    // обновление событий в мире игры
+    var idDeleteObject;
+    function randomPositionX() {
+        return Math.floor(Math.random() * (400 - 50 + 1) + 50);
+    }
+    function GameObjectGenerator() {
+        var rand = Math.floor(Math.random() * types.length);
+        return types[rand];
+    }
+    /**
+     * Обновление событий в мире игры
+     */
     function updateGameArea() {
         var x, height, gap, minHeight, maxHeight, minGap, maxGap;
-        
-        if (deleteObject) {
-            delete obstacles[deleteObject.id];
-            deleteObject = null;
+        // очистка объектов
+        if (idDeleteObject) {
+            delete allObjects[idDeleteObject];
+            idDeleteObject = null;
         }
-        for (var obstacle in obstacles) {
-            if (player.crashWith(obstacles[obstacle])) {
-                player.energy -= 10;
-                deleteObject = obstacles[obstacle];
+        // проверка событий и объектов вышедших за рамки игры
+        for (var id in allObjects) {
+            var object = allObjects[id];
+            if (object.x + object.width < 0) {
+                idDeleteObject = id;
+                break;
+            } else if (player.crashWith(object)) {
+                if (object.effect) {
+                    var keySave;
+                    for (var key in object.effect) {
+                        player[key] += object.object[key];
+                        keySave = key;
+                        if (key == "time") {
+                            function fun() {
+                                player[keySave] -= object[keySave];
+                            } 
+                            setTimeout(func, object[key] * 1000);
+                        }
+                        console.log(player)
+                        debugger
+                    }
+                }
+                player.energy += object.energy;
+                idDeleteObject = id;
                 break;
             }
             if (player.energy <= 0) {
                 return;
             }
-        };
+        }
+
         gameArea.clear();
         gameArea.frameNo += 1;
-        var bottomObstacle = 0;
-        if (gameArea.frameNo == 1 || everyinterval(300)) {
-            x = canvas.width;
-            minHeight = 10;
-            maxHeight = 200;
+        // уменьшение энергии
+        if (everyinterval(100)) {
+            player.energy -= 1;
+        }
+        // расстановка объектов
+        if (gameArea.frameNo == 1 || everyinterval(200)) {
+            minHeight = canvas.height / 20;
+            maxHeight = canvas.height / 10;
             minWidth = canvas.width / 20;
             maxWidth = canvas.width / 10;
-            height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
-            width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
-            minGap = player.height * 2;
-            maxGap = player.height * 10;
-            gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
-            obstacles[indexObject] = Object.assign(new Сomponent(width, height, "black", x, 0), { id: indexObject});
-            bottomObstacle = canvas.height - height - gap;
-            if (bottomObstacle > 0) {
-                obstacles[++indexObject] = Object.assign(new Сomponent(width, bottomObstacle, "red", x, height + gap), { id: indexObject});
+            minGap = player.height * 1.5;
+            maxGap = player.height * 2;
+            var positionY = 0;
+            var index = 0;
+            while (positionY < canvas.height && index < 5) {
+                x = canvas.width + randomPositionX();
+                height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
+                width = Math.floor(Math.random() * (maxWidth - minWidth + 1) + minWidth);
+                allObjects[indexObject] = Object.assign(new Сomponent(width, height, GameObjectGenerator(), x, positionY), { id: indexObject});
+                gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+                positionY += (height + gap);
+                allObjects[indexObject].update();
+                indexObject++;
+                index++;
             }
-        else
-            console.log(bottomObstacle + "!")
         }
-        for (var item in obstacles) {
-            obstacles[item].x += -1;
-            obstacles[item].update();
-        }; 
+        for (var id in allObjects) {
+            allObjects[id].x -= player.speed;
+            allObjects[id].update();
+        }
         playerScore.text = "Score: " + gameArea.frameNo;
         playerEnergy.text = "Energy: " + player.energy;
         playerScore.update();
